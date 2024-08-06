838b42850f2f4b5a4fc2802c1bd25098106a3195
Daria Borisiak <daria.borisiak@gmail.com>
2024-08-06 23:46:43 +0200

    Complete excercises

--- src/main/scala/recfun/RecFun.scala
+++ src/main/scala/recfun/RecFun.scala
@@ -1,5 +1,7 @@
 package recfun
 
+import scala.annotation.tailrec
+
 object RecFun extends RecFunInterface:
 
   def main(args: Array[String]): Unit =
@@ -12,14 +14,28 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean =
+    @tailrec
+    def isBalanced(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (chars.head == '(') isBalanced(chars.tail, open + 1)
+      else if (chars.head == ')') open > 0 && isBalanced(chars.tail, open - 1)
+      else isBalanced(chars.tail, open)
+    }
+
+    isBalanced(chars, 0)
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
